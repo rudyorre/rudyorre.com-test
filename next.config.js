5480eeaaf59c55d3cf91b7843df5ef7798333114
Rudy Orre <44660069+rudyorre@users.noreply.github.com>
2024-01-01 02:38:06 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/rudyorre.com",
+    basePath: "/rudyorre.com-test",
 
     /**
      * Disable server-based image optimization. Next.js does not support
